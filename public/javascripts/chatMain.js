134e9d8d06a87ae9b50dd338c510edc49c1adbc9
vonfrohn <73224292+vonfrohn@users.noreply.github.com>
2021-05-03 10:56:57 +0200

    Frederik

--- public/javascripts/chatMain.js
+++ public/javascripts/chatMain.js
@@ -102,21 +102,21 @@ function playersInRoom(users) {
   return playersInRoom;
 }
 
-// function enableStartButton(users) {
-//   let players = playersInRoom(users);
-//   console.log('Hello1');
-//   const button = document.getElementById("start-btn");
-//   if(button !== null){
-//   if (players.length >= 2 && players.length <= 4) {
-//     button.disabled = false;
-//     //start(users);
-//     console.log("Test succes"); 
-//   } else {
-//     button.disabled = true;
-//     console.log("Test failed");
-//   }
-//   }
-// }
+function enableStartButton(users) {
+  // let players = playersInRoom(users);
+  // console.log('Hello1');
+  // const button = document.getElementById("start-btn");
+  // if(button !== null){
+  // if (players.length >= 2 && players.length <= 4) {
+  //   button.disabled = false;
+  //   //start(users);
+  //   console.log("Test succes"); 
+  // } else {
+  //   button.disabled = true;
+  //   console.log("Test failed");
+  // }
+  // }
+}
 
 function leaveRoom() {
   // location.href = "/lobby";
